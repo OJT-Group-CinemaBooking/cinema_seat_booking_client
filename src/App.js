a8cc61262cb13cfd6e92287c66438482e89915f4
YanNaingToe <yannaing.npttn@gmail.com>
2024-01-10 21:24:44 +0630

    finish seat and checkout page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Layout from "./components/ui/layout";
-import SeatBookingPage from "./components/page/SeatBookingPage";
-import CheckOutPage from "./components/page/CheckOutPage";
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout/>}>
-        <Route index element={<SeatBookingPage/>} />
-        <Route path="payment" element={<CheckOutPage/>}/>
+       
       </Route>
     </Routes>
   );
