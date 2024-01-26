3cc5d2a220e2e91dad4485da6a5959fd78a7c465
YanNaing009 <145839510+YanNaing009@users.noreply.github.com>
2024-01-26 23:05:14 +0630

    Confirmation page (#19)
    * finish login and confirmation
    
    * finish seat
    
    * finish fix confirmation Page

--- src/features/confirmation/Confirmation.jsx
+++ src/features/confirmation/Confirmation.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import classes from './Confirmation.module.css'
 import { CheckCircle } from 'react-bootstrap-icons'
-import { Container } from 'react-bootstrap'
+import { Col, Container, Row } from 'react-bootstrap'
 
 const Confirmation = () => {
+
+    const ticketId = 1
+
+    const today = new Date().toISOString().split('T')[0]
+
+    const seatPrice = 28000
+    const total = (seatPrice + 500 + 100)
   return (
     <>
         <h3 className={classes.headder}>Booking Confirmation</h3>
         <Container className={classes.date}>
-            <p>11/1/2024</p>
+            <p>{today}</p>
         </Container>
 
         <Container className={classes.success}>
@@ -16,17 +23,93 @@ const Confirmation = () => {
         </Container>
 
         <Container className={classes.ticket}>
-            <div className={classes.ticket_details}>
-                <h4>Migration</h4>
-                <p>Mingalar Cinema, Therator - 1</p>
-                <p><span>20:00</span> | <span>20/1/2024</span></p>
-                <p>Nomal - <span>C3,C4</span></p>
-                <p>Premium - <span>F6</span></p>
-                <h5>Amount Paid - <span>28000 MMK</span></h5>
-            </div>
-            <div className={classes.ticket_qr}>
-                <img src="https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=1" alt="QR code" />
-            </div>
+            <Row className={classes.first_row}>
+                <Col className={classes.ticket_details}>
+                    <h4>
+                        Booking ID : {ticketId}
+                    </h4>
+                    <h4 className={classes.movie_name}>Migration</h4>
+                    <p>ENG ( MMR )</p>
+                    <p>Mingalar Cinema</p>
+                    <p>Therator - 1</p>
+
+
+                    <p>Nomal - <span>C3,C4</span></p>
+                    <p>Premium - <span>F6</span></p>
+                </Col>
+                <Col className={classes.date_time}>
+                    <p>
+                        Friday, 26th January
+                    </p>
+                    <p style={{marginBottom : '1rem'}}>
+                        8:00 PM
+                    </p>
+                    <img src={`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${ticketId}`} alt="QR code" />
+                </Col>
+            </Row>
+            
+            <hr className={classes.hr_style}/>
+
+            <Row>
+                <Col className={classes.order_summary}>
+                    <h4>Order Summary</h4>
+                </Col>
+            </Row>
+
+            <hr className={classes.hr_style_dotted}/>
+           
+            
+                <Row>
+                    <Col className={classes.fee}>
+                        <h5>Total Seat Price</h5>
+                    </Col>
+                    
+                    <Col className={classes.fee_price}>
+                        <p>28000 MMK</p>
+                    </Col>
+                </Row>
+                <Row className={classes.extra_fee}>
+                    <Col className={classes.fee}>
+                        <p>Convenience Fee</p>
+                    </Col>
+                    
+                    <Col className={classes.fee_price}>
+                        <p>500 MMK</p>
+                    </Col>
+                </Row>
+                <Row className={classes.extra_fee}>
+                    <Col className={classes.fee}>
+                        <p>Service Tax</p>
+                    </Col>
+                    
+                    <Col className={classes.fee_price}>
+                        <p>100 MMK</p>
+                    </Col>
+                </Row>
+            
+            <hr className={classes.hr_style_dotted}/>
+            <Row>
+                <Col className={classes.fee}>
+                    <h4>Total</h4>
+                </Col>
+                <Col className={classes.fee_price}>
+                    <p>{total} MMK</p>
+                </Col>
+            </Row>
+
+            <hr className={classes.hr_style}/>
+
+            <Row className={classes.note}>
+                <h4>Important Notes:</h4>
+            </Row>
+            <Row className={classes.note}>
+                    <p>Ticket once ordered cannot be exchanged, cancelled or refounded.</p>
+                    <p>Children aged 3 years and above will require a separate ticket.</p>
+                    <p>The 3D glasses will be available at the cinema for 3D films and must be returned before you exit the premises. 3D Glasses are chargeable (refundable/non-refundable) as per individual cinema policies.</p>
+                    <p>Items like laptop, cameras, knifes, lighter, match box, cigarettes, firearms and all types of inflammable objects are strictly prohibited.</p>
+                    <p>Items like carrybags eatables, helmets, handbags are not allowed inside the theaters are strictly prohibited. kindly deposit at the baggage counter of mall/cinema.</p>
+            </Row>
+
         </Container>
     </>
   )
